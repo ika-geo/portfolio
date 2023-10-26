2beefcc52554077b465f225992b2c46a085b9eb5
Your Name <you@example.com>
2023-10-26 22:24:43 +0400

    mongdo

--- src/blocks/Portfolio.tsx
+++ src/blocks/Portfolio.tsx
@@ -16,7 +16,7 @@ const portfolioItems: PortfolioItemInterface[] = [
         img: 'portfolioIMG (11).png',
         url: 'https://posts-client-navy.vercel.app/',
         github: 'https://github.com/ika-geo/posts',
-        technologies: ['React, Node, Express, Mongo, SCSS, Javascript'],
+        technologies: ['React, Node, Express, MongoDB, SCSS, Javascript'],
         MERN: true
     },
     {

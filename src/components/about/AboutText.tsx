823e2631cf48ec6222942fe9e008f3f358d67e1d
Your Name <you@example.com>
2023-10-28 22:17:05 +0400

    some text

--- src/components/about/AboutText.tsx
+++ src/components/about/AboutText.tsx
@@ -1,12 +1,12 @@
 const AboutText = () => {
     return (
         <div className='about__text text'>
-            <p>Hi, I'm Irakli, a passionate Full-Stack MERN Developer with a keen eye for creating seamless and efficient web applications. My journey in web development has led me to specialize in the MERN stack, enabling me to build dynamic and responsive solutions that cater to the modern digital landscape. I'm also well-versed in Redux Toolkit for advanced state management.:</p>
+            <p>Hi, I'm Irakli, a passionate Full-Stack (MERN) Developer with a keen eye for creating seamless and efficient web applications. My journey in web development has led me to specialize in the MERN stack, enabling me to build dynamic and responsive solutions that cater to the modern digital landscape:</p>
 
             <ul>
 
                 <li>
-                    <b className='coloredText2'>Experience: </b>without commercial. My experience as a MERN Developer has equipped me with the skills to craft end-to-end web applications that deliver on both the front-end and back-end. With a solid foundation in MongoDB, Express.js, React, and Node.js, I have successfully implemented RESTful APIs, managed data storage, and ensured the cross-browser and cross-platform compatibility of web projects.
+                    <b className='coloredText2'>Experience: </b>Without commercial. My experience as a MERN Developer has equipped me with the skills to craft end-to-end web applications that deliver on both the front-end and back-end. With a solid foundation in MongoDB, Express.js, React, and Node.js, I have successfully implemented RESTful APIs, managed data storage, and ensured the cross-browser and cross-platform compatibility of web projects.
                 </li>
 
                 <li> <b className='coloredText2'>Skills: </b>Proficiency in the MERN stack, with a good understanding of MongoDB, Express.js, React, Node.js and relevant libraries and technologies. Expertise in creating responsive user interfaces and implementing user-friendly features. Seasoned in state management, utilizing Redux Toolkit to enhance the predictability and reliability of complex applications. Committed to continuous learning, refining my JavaScript skills, and staying updated with the latest web development trends. Adept problem solver and a fast learner. English level: B2.

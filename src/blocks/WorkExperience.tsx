12941ad7ba937c0b8cda0ed07340a59264542cb4
ikascorp <ikagele.scorp@gmail.com>
2024-06-07 16:12:06 +0400

    work experience items place

--- src/blocks/WorkExperience.tsx
+++ src/blocks/WorkExperience.tsx
@@ -10,13 +10,12 @@ interface WorkItem{
 
 const workItems: WorkItem[] = [
     {
-        years: [2014, 2016],
-        duties: 'Search for descriptions and pictures of products on the Internet, create descriptions, edit pictures, add to the site.',
-        position: 'Digital Content Manager',
-        companyName: "Ecommerce be.ge",
-        companyLink: "https://be.ge/en",
+        years: [2023, 'present'],
+        duties: 'Engaged in full-stack development, where I manage CRUD operations, create features, components, and handle debugging for seamless user interactions. I work within the MERN stack, utilizing Redux Toolkit, Tailwind, TypeScript, and third-party npm libraries. Actively collaborating with developers for code improvement and quality enhancement. I contribute to new features, ensuring positive user experiences.',
+        position: 'Full stack developer (MERN)',
+        companyName: "Codeunity",
+        companyLink: "https://www.codeunity.in/"
     },
-
     {
         years: [2017, 2024],
         duties: 'Search for descriptions and pictures of products on the Internet, create descriptions in three languages (Georgian, Russian, English), edit pictures, add to the site.',
@@ -25,11 +24,11 @@ const workItems: WorkItem[] = [
         companyLink: "https://www.domino.com.ge/en/"
     },
     {
-        years: [2023, 'present'],
-        duties: 'Engaged in full-stack development, where I manage CRUD operations, create features, components, and handle debugging for seamless user interactions. I work within the MERN stack, utilizing Redux Toolkit, Tailwind, TypeScript, and third-party npm libraries. Actively collaborating with developers for code improvement and quality enhancement. I contribute to new features, ensuring positive user experiences.',
-        position: 'Full stack developer (MERN)',
-        companyName: "Codeunity",
-        companyLink: "https://www.codeunity.in/"
+        years: [2014, 2016],
+        duties: 'Search for descriptions and pictures of products on the Internet, create descriptions, edit pictures, add to the site.',
+        position: 'Digital Content Manager',
+        companyName: "Ecommerce be.ge",
+        companyLink: "https://be.ge/en",
     },
 ]
 
@@ -50,7 +49,7 @@ const WorkExperience = () => {
                 <div className="workExperience__block">
                     <h1 className="workExperience__title center title">WORK EXPERIENCE</h1>
                     {workItems.map(item =>
-                        <div className="workItem flex">
+                        <div key={item.companyName} className="workItem flex">
                             <h2 className="years subTitle">{item.years[0]}-<span className={item.years[1]==='present'?'coloredText2':''}>{item.years[1]}</span></h2>
                             <p className="duties text">{item.duties}</p>
                             <div className="position">
